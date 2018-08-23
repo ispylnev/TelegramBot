4a6e747651f5c280955f79f1698f1079cada8c87
Ivan <ivan_pylnev@mail>
2018-08-23 09:13:17 +0300

    test

--- src/main/java/Utils/MyDate.java
+++ src/main/java/Utils/MyDate.java
@@ -40,7 +40,8 @@ public class MyDate {
              DateTimeFormatter formatterProxy = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
             LocalDateTime start = LocalDateTime.parse(beginTime.substring(0,23), formatterProxy);
             LocalDateTime end = LocalDateTime.parse(endTime.substring(0,23), formatterProxy);
-             return MyDate.duration = Duration.between(start, end).toSeconds();
+//             return MyDate.duration = Duration.between(start, end).toSeconds();
+             return 0;
 
         } catch (DateTimeException e){
 
